7bd358baf2cdd5b0329551488739c428c93f17e0
Andrea Vaccari <avaccari@middlebury.edu>
2023-10-17 11:23:40 -0400

    Segentation costs depend on availability of cytoplasm

--- segment.py
+++ segment.py
@@ -1,10 +1,42 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  #! debug
 import numpy as np
 import skimage.measure as ski_mea
 import skimage.morphology as ski_mor
 from colorama import Fore, Style
 
+
 import os_utils
+
+
+class Cost:
+    def __init__(self):
+        pass
+
+    def add(self, cost_dict):
+        for k, v in cost_dict.items():
+            if not hasattr(self, k):
+                setattr(self, k, [])
+            getattr(self, k).append(v)
+
+    def get(self):
+        return self.__dict__
+
+    def show(self, ax=None):
+        if ax is None:
+            plt.ion()
+            plt.figure()
+            for k, v in self.get().items():
+                plt.scatter(range(len(v)), v, s=3, label=k)
+
+            plt.yscale("log")
+            plt.legend()
+            plt.show()
+        else:
+            for k, v in self.get().items():
+                ax.scatter(range(len(v)), v, s=3, label=k)
+            ax.set_yscale("log")
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
 
 
 class NucleiSegmentation:
@@ -13,31 +45,76 @@ class NucleiSegmentation:
         self.ch_id = ch_id
         self.overwrite = overwrite
         self.out_dir = out_dir
+        self.costs = {}
 
-    def _cost(self, t, temp_region, temp_values, temp_cytoplasm=None):
-        # Threshold within the Voronoi cell
+    def _find_rows_cols(self, rng, aspect_ratio=1.4):
+        rows = 1
+        cols = 1
+        while rows * cols < rng:
+            if cols / rows > aspect_ratio:
+                rows += 1
+            else:
+                cols += 1
+        return rows, cols
+
+    def plot_costs(self, aspect_ratio=1.4):
+        rows, cols = self._find_rows_cols(len(self.costs), aspect_ratio)
+        plt.ion()
+        fig, axs = plt.subplots(rows, cols, sharex=True, sharey=True)
+        for ax, (k, v) in zip(axs.flatten(), self.costs.items()):
+            v.show(ax=ax)
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.tight_layout()
+        plt.show()
+
+    def _cost(self, t, temp_region, temp_values, temp_cytoplasm=None, normalize=False):
+        # Assumes nuclei to be brighter than background
+        # --- Nuclei ---
+        # Threshold volume within the Voronoi cell
         msk = np.zeros_like(temp_region, dtype="bool")
         msk[temp_region] = temp_values[temp_region] >= t
-        # if temp_cytoplasm is not None:
-        #     msk_cyto = np.zeros_like(temp_region, dtype="bool")
-        #     msk_cyto[temp_region] = temp_cytoplasm[temp_region] >= t
 
-        # Outside of thresholded area within the Voronoi cell
-        out_idx = np.logical_and(~msk, temp_region)
-        out_variance = self._evaluate_variance_in_volume(
-            out_idx, temp_values, normalize=False
-        )
-
-        # Inside of thresholded area within the Voronoi cell
+        # Nucleus inside of volume
         in_idx = np.logical_and(msk, temp_region)
         in_variance = self._evaluate_variance_in_volume(
-            in_idx, temp_values, normalize=False
+            in_idx, temp_values, normalize=normalize
         )
 
-        # surface_idx = np.logical_xor(
-        #     ski_mor.binary_dilation(msk, footprint=ski_mor.ball(1)), msk
-        # )
-        # surface = surface_idx.sum()
+        # Nucleus outside of volume
+        out_idx = np.logical_and(~msk, temp_region)
+        out_variance = self._evaluate_variance_in_volume(
+            out_idx, temp_values, normalize=normalize
+        )
+
+        # Cost if only nucleus is considered
+        cost = out_variance + in_variance
+
+        # --- Cytoplasm ---
+        if temp_cytoplasm is not None:
+            # # Threshold within the Voronoi cell
+            # msk_cyto = np.zeros_like(temp_region, dtype="bool")
+            # msk_cyto[temp_region] = temp_cytoplasm[temp_region] >= t
+
+            # Cytoplasm outside of volume (expected inside the cytoplasm)
+            in_cyto_variance = self._evaluate_variance_in_volume(
+                out_idx, temp_cytoplasm, normalize=normalize
+            )
+            # Cytoplasm inside of volume (expected outside the cytoplasm)
+            out_cyto_variance = self._evaluate_variance_in_volume(
+                in_idx, temp_cytoplasm, normalize=normalize
+            )
+
+            # Cost if also cytospasm is considered
+            cost = out_variance + out_cyto_variance
+            # cost += out_cyto_variance
+
+        surface_idx = np.logical_xor(
+            ski_mor.binary_dilation(msk, footprint=ski_mor.ball(1)), msk
+        )
+        surface = surface_idx.sum()
+
+        # # Minimize surface as well
+        cost -= surface
 
         # volume = in_idx.sum()
         # volume_val = temp_values[in_idx].sum()
@@ -45,9 +122,30 @@ class NucleiSegmentation:
         table = ski_mea.regionprops(ski_mea.label(msk))
         objects = len(table)
 
+        # cost += 0 if (objects == 1) else np.inf
+
+        # Store costs in dictionary
+        cost_dict = {
+            "out_variance": out_variance,
+            "in_variance": in_variance,
+            # "in + out": in_variance + out_variance,
+            "in_cyto_variance": in_cyto_variance,
+            "out_cyto_variance": out_cyto_variance,
+            # "out + out_cyto": out_variance + out_cyto_variance,
+            # "in_cyto + out_cyto": in_cyto_variance + out_cyto_variance,
+            # "in + out + out_cyto": in_variance + out_variance + out_cyto_variance,
+            # "in + out_cyto": in_variance + out_cyto_variance,
+            # "out + in_cyto": out_variance + in_cyto_variance,
+            "surface": surface,
+            # "volume": volume,
+            # "volume_val": volume_val,
+            # "objects": objects,
+            "cost": cost,
+        }
         return (
-            out_variance + in_variance + 0 if (objects == 1) else np.inf,
+            cost,
             objects,
+            cost_dict,
         )
 
     def _evaluate_variance_in_volume(self, indexes, values, normalize=False):
@@ -62,14 +160,15 @@ class NucleiSegmentation:
 
         return variance
 
-    def _find_min_threshold(self, start, temp_region, temp_values, temp_cytoplasm=None):
-        # costs = []  #! Debug
+    def _find_min_threshold(self, temp_region, temp_values, temp_cytoplasm=None):
+        costs = Cost()
         min_cost = np.inf
         threshold_min = 0
-        for threshold in range(start, 256):
-            cost, objects = self._cost(
+        for threshold in range(1, 256):
+            cost, objects, cost_dict = self._cost(
                 threshold, temp_region, temp_values, temp_cytoplasm
             )
+            costs.add(cost_dict)
             # Assumption: we are increasing the threshold so, if we have no
             # objects at a certain threshold, we will never have objects at
             # higher thresholds.
@@ -79,13 +178,12 @@ class NucleiSegmentation:
                 min_cost = cost
                 threshold_min = threshold
             print(
-                f"\rThreshold: {threshold:3d} => Cost: {cost:12.2f} (Optimal: {threshold_min:3d} => {min_cost:12.2f})",
+                f"\rThreshold: {threshold:3d} => Cost: {cost:12.2f} Objects: {objects:3d} (Optimal: {threshold_min:3d} => {min_cost:12.2f})",
                 end="",
                 flush=True,
             )
-            # costs.append(cost)  #! Debug
-        # plt.scatter(range(start, start + len(costs)), costs, s=1)  #! Debug
-        return threshold_min
+
+        return threshold_min, costs
 
     def _find_region_limits(self, current_region):
         # Find extension of the region of interest within the whole volume
@@ -152,12 +250,11 @@ class NucleiSegmentation:
             return False, "center out of region"
         if check.sum() > 1:  # More than one center inside the region of interest
             return False, "multiple centers in region"
-        if self._touch_edges(temp_mask_open):  # Region touches the border of the volume
-            return False, "region touches border"
+        # if self._touch_edges(temp_mask_open):  # Region touches the border of the volume
+        #     return False, "region touches border"
         return True, "good"
 
     def _segment(self, regions, values, centers, cytoplasm=None):
-        # plt.figure()  #! Debug
         labels = np.zeros_like(regions)
         start = 1
         for lbl in range(start, regions.max() + start):
@@ -179,14 +276,17 @@ class NucleiSegmentation:
             # the region of interest
             temp_region = current_region[z_min:z_max, y_min:y_max, x_min:x_max]
             temp_values = values[z_min:z_max, y_min:y_max, x_min:x_max]
-            temp_cytoplasm = None
-            if cytoplasm is not None:
-                temp_cytoplasm = cytoplasm[z_min:z_max, y_min:y_max, x_min:x_max]
+            temp_cytoplasm = (
+                None
+                if cytoplasm is None
+                else cytoplasm[z_min:z_max, y_min:y_max, x_min:x_max]
+            )
 
             # Optimize the threshold to identify the nucleus within the region
-            threshold_min = self._find_min_threshold(
-                start, temp_region, temp_values, temp_cytoplasm
+            threshold_min, costs = self._find_min_threshold(
+                temp_region, temp_values, temp_cytoplasm
             )
+            self.costs[lbl] = costs
 
             # Create a mask that isolates the nucleus within the region of interest
             temp_mask = np.zeros_like(temp_region, dtype="bool")
@@ -213,11 +313,10 @@ class NucleiSegmentation:
                 print(f"{Fore.GREEN} ✓ ({reason}){Style.RESET_ALL}")
             else:
                 print(f"{Fore.RED} ✕ ({reason}){Style.RESET_ALL}")
-        # plt.show()  #! Debug
+
         return labels
 
     def segment(self, labels, values, centers, cytoplasm=None, write_to_tiff=False):
-        # plt.ion()  #! Debug
         print(f"Segmenting {self.ch_id}")
         labels = os_utils.store_to_npy(
             self._segment,
